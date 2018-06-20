f9fde1fe44ecadc47e6ed623886ae3240fca283a
Viko Dev <viko.dev.z@gmail.com>
2018-06-20 18:08:27 -0400

    Passing and Using Data with Event Binding
    1. Event Binding for input event of text field, we call to a TS method called "onUpdateServerName($event)"
    2. We initializa a property on TS file and delacre the method,  where we proceed to store it

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created!'
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout( () => {
@@ -22,4 +23,8 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = 'Server was created';
   }
 
+  onUpdateServerName(event: Event) {
+     console.log(event);
+     this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
