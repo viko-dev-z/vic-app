a01d75b871c257d724d4aee3dbbb0d405e789726
Viko Dev <viko.dev.z@gmail.com>
2018-06-20 12:16:51 -0400

    Change the selector to be an attribute by enclosing into square brackets
    selector changing from an element to be an attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-     h3 {
-       color: blue;
-     }
-  ` ]
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
 }
