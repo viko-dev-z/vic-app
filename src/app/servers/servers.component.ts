0ca23bef1520c2321b83326069b56972249b94a8
Victor Paredes <vico.sistemas@gmail.com>
2018-06-23 23:48:49 -0400

    Usage example of directive ngIf
    for the conditional message displayed when Add Server button is clicked

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
     setTimeout( () => {
@@ -20,7 +21,8 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
-  onCreateServer(){
+  onCreateServer() {
+    this.serverCreated = true
     this.serverCreationStatus = 'Server was created Name is ' + this.serverName;
   }
 
