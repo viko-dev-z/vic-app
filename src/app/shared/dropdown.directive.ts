862ac67e28ca0763212bb6be329873c02d5fa09f
Victor Paredes <vico.sistemas@gmail.com>
2018-07-04 14:29:32 -0400

    Building and using a Dropdown directive
    dropdown.directive.ts
    // logic that listen to clicks and toggles some property. (attach css classes)

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import {Directive, HostBinding, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+
+export class DropdownDirective {
+  // logic that listen to clicks and toggles some property. (attach css classes)
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toogleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
+}
