6f307db69e0c9e4dea2f51b95a4cf739ebf8f160
Victor Paredes <vico.sistemas@gmail.com>
2018-07-05 02:55:05 -0400

    Adding Shopping List Service
    adding the service provider in the app module and then replacing a lot of old output event emitters with service communication, for viewing shopping list and adding ingredients

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,21 @@
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 1),
+    new Ingredient('Cabbages', 7)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 
 }
