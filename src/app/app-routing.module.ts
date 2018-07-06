5bebc48d94d9ea53dbecd4baacdc1b267be3283e
Victor Paredes <vico.sistemas@gmail.com>
2018-07-06 00:47:15 -0400

    Adding a new navigation menu for calendar
    ng g c calendar/calendar-view --spec false
    also modifying the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import {CalendarViewComponent} from './calendar/calendar-view/calendar-view.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
@@ -14,7 +15,8 @@ const appRoutes: Routes = [
       {path: ':id', component: RecipeDetailComponent },
       {path: ':id/edit', component: RecipeEditComponent }
     ]},
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'calendar', component: CalendarViewComponent }
 ];
 
 @NgModule({
